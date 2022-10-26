fc6f239102652751f26ab1ff0201b597d4789b02
saharmoradik <saharmoradik@gmail.com>
2022-10-26 11:42:41 +0200

    footer component

--- src/App.vue
+++ src/App.vue
@@ -3,15 +3,18 @@
     <v-main>
       <SideBar />
     </v-main>
+    <FooterComponent />
   </v-app>
 </template>
 
 <script>
 import SideBar from "./components/SideBar.vue";
+import FooterComponent from "./components/FooterComponent.vue";
 export default {
   name: "App",
   components: {
     SideBar,
+    FooterComponent,
   },
 };
 </script>
